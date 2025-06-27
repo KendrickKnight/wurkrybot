9144d41265d2565ac709c9fa1c6b6cd377c0a810
Ehsan Langroudi <89141575+KendrickKnight@users.noreply.github.com>
2025-06-27 20:13:10 +0330

    lobby cog test

---
+++ cogs/cog_lobby.py
@@ -0,0 +1,126 @@
+import discord
+from discord.ext import commands
+from time import sleep
+import json
+import msg_funcs as msf
+
+class Lobby(commands.Cog):
+    
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command(help = "[Member] () Shows all lobbies at this singular moment")
+    async def lobby(ctx):
+        with open("../server_settings.json", "r") as ss:
+            server_setting = json.load(ss)
+        with open("../data.json", 'r') as dataFile:
+            data = json.load(dataFile)
+            text, view = await msf.lobby_report(data["lobbies"],server_setting[str(ctx.guild.id)])
+            await ctx.send(text,view=view)
+
+
+    @commands.has_permissions(administrator=True)
+    @commands.command(help = "[Admin] () purge a channel's messages, start monitoring LWG lobbies")
+    async def lobby_start(ctx):
+    
+        #Initial channel clear and message
+        def check(msg):
+            return True  # All messages
+        await ctx.channel.purge(limit=None, check=check, bulk=True)
+        await ctx.send("Very well, i'l start in 5 seconds.")
+        sleep(5)
+
+
+        role_ranked = discord.utils.get(ctx.guild.roles, name="ranked")
+
+        with open("../server_settings.json", "r") as settingfile:
+            setting = json.load(settingfile)
+            guild_setting = setting[str(ctx.guild.id)]
+
+        global stop_monitor
+        role_ranked = discord.utils.get(ctx.guild.roles, name="ranked")
+        last_ranked_search_status = False
+        
+        notif_list_setting = []
+        notif_list = []
+        for i in guild_setting["notifications"]["custom"]:
+            notif_list.append(False)
+            notif_list_setting.append(False)
+
+        
+        while True:
+            with open('../data.json', 'r') as dataFile:
+                data = json.load(dataFile)
+                messages = [msg async for msg in ctx.channel.history(limit=50)]
+                
+                
+                if stop_monitor:
+                    await ctx.send("Monitoring Stopped!", view=None)
+                    stop_monitor = False
+                    return
+
+                # Makes sure that only 2 messages are in the channel: role giver and lobby_monitor
+                for i in range(len(messages)):
+                    if messages[i].author != commands.user:
+                        await messages[i].delete()
+                    elif messages[i].author == commands.user and i != 0:
+                        await messages[i].delete()
+                        
+                if messages[-1].author != commands.user:
+                    await messages[-1].delete()
+                else:
+                    text, view = await msf.monitor_report(data, role_ranked, guild_setting)
+                    await messages[-1].edit(content=text ,view=view)
+                
+                
+            #Notifications
+                #Ranked Notification
+                if guild_setting["notifications"]["ranked"] == True:
+                    if data["searching"] == True and last_ranked_search_status == False:
+                        last_ranked_search_status = True
+                        commands.ranked_notice_message = await ctx.send(f"{role_ranked.mention}")
+                        await commands.ranked_notice_message.delete()
+                    elif data["searching"] == False:
+                        last_ranked_search_status = False
+                        
+                #Custom_role Notifications
+                count = 0
+                for i in guild_setting["notifications"]["custom"]:
+                    gs = guild_setting["notifications"]["custom"]
+                    map_exist = False
+                    for k in data["lobbies"]:
+                        if gs[i]["enabled"] == True and i.lower() in k["map"].lower():
+                            map_exist = True
+                    
+                    if map_exist:
+                        notif_list[count] = True
+                    else:
+                        notif_list[count] = False
+                    count += 1
+                
+
+                notif_list_count = 0
+                for i in guild_setting["notifications"]["custom"]:
+                    if notif_list[notif_list_count] == True and notif_list_setting[notif_list_count] == False:
+                        notif_list_setting[notif_list_count] = True
+                        commands.ranked_notice_message = await ctx.send(f"{discord.utils.get(ctx.guild.roles, name = i).mention}")
+                        await commands.ranked_notice_message.delete()
+                    elif notif_list[notif_list_count] == False:
+                        notif_list_setting[notif_list_count] = False
+                    notif_list_count += 1
+                notif_list_count = 0
+                
+
+
+            sleep(2)
+        
+    @commands.has_permissions(administrator=True)
+    @commands.command(help="[Admin] () stops !lobby_start command")
+    async def lobby_stop(ctx):
+        global stop_monitor
+        stop_monitor =  True
+
+    
+
+async def setup(bot):
+    await bot.add_cog(Lobby(bot))
---
+++ cogs/msg_funcs.py
@@ -0,0 +1,88 @@
+import discord 
+from discord.ext import commands
+
+lwg_link = "https://littlewargame.com/play/"
+emoji_running = ":man_running_facing_right:"
+emoji_lock = ":lock:"
+
+
+
+# Commoun Codes 
+def link_button_view():
+    view = discord.ui.View()
+    item = discord.ui.Button(style=discord.ButtonStyle.link, label='Join', url=lwg_link)
+    view.add_item(item=item)
+ 
+
+async def lobby_report(data, setting):
+        # Generating message text
+    lobby_text = "\n **Lobbies:** \n"
+    customs_text = ""
+
+    if len(setting["notifications"]["custom"]) > 0:
+            customs_text += "\n **Filtered Lobbies:**\n"
+
+    for i in data:
+        if len(setting["notifications"]["custom"]) > 0:
+            for k in setting["notifications"]["custom"]:
+                if k.lower() in i["map"].lower():
+                    customs_text += f"> {i["name"]} {i["player_count"]}{emoji_running if i["running"]==True else ""}{emoji_lock if i["locked"]==True else "" + "\n"}"
+                else:
+                    lobby_text += f"> {i["name"]} {i["player_count"]}{emoji_running if i["running"]==True else ""}{emoji_lock if i["locked"]==True else "" + "\n"}"
+
+                
+        else: lobby_text += f"\n > {i["name"]} {i["player_count"]}{emoji_running if i["running"]==True else ""}{emoji_lock if i["locked"]==True else ""}"
+                
+    
+    text = lobby_text + customs_text
+
+    return text, link_button_view()
+        
+
+async def monitor_report(data, ranked, setting):
+
+    connection_text = "**Reporter Status:**"
+    ranked_text = "**Ranked** \n"
+    lobby_text = "\n **Lobbies:** \n"
+    customs_text = ""
+
+    if data["connection"]:
+        connection_text += " :green_circle: \n"
+    else:
+        connection_text += " :red_circle: \n"
+
+    if data["searching"] == True:
+        ranked_text += f"{ranked.mention} `Someone's Searching!` \n"
+    else: 
+        ranked_text += f"`No one is searching. START YOUR CRUSADE!` \n"
+        last_ranked_search_status = False
+
+    if len(setting["notifications"]["custom"]) > 0:
+            customs_text += f"\n **Filtered Lobbies:**\n"
+
+    for i in data["lobbies"]:
+        if len(setting["notifications"]["custom"]) > 0:
+            for k in setting["notifications"]["custom"]:
+                if k.lower() in i["map"].lower():
+                    customs_text += f"> {i["name"]} {i["player_count"]}{emoji_running if i["running"]==True else ""}{emoji_lock if i["locked"]==True else "" + "\n"}"
+                else:
+                    lobby_text += f"> {i["name"]} {i["player_count"]}{emoji_running if i["running"]==True else ""}{emoji_lock if i["locked"]==True else "" + "\n"}"
+
+                
+        else: lobby_text += f"\n > {i["name"]} {i["player_count"]}{emoji_running if i["running"]==True else ""}{emoji_lock if i["locked"]==True else ""}"
+                
+    
+    text = connection_text + ranked_text + lobby_text + customs_text
+
+    return text, link_button_view()
+
+async def customs_notif(ctx, data):
+    for i in data:
+        role = await discord.utils.get(ctx.guild.roles, name=i)
+        enabled = data[i]["notify"]
+        map_name = data[i]["map"]
+
+        if enabled:
+            bot.ranked_notice_message = await ctx.send(f"{role.mention}")
+            await bot.ranked_notice_message.delete()
+
